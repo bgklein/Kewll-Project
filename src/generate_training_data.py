4e6e49e1c15c8311611674c87b0cfc1a835f281a
fjiang23 <fjiang23@wisc.edu>
2017-04-10 12:55:20 -0500

    Update generate_training_data.py

--- src/generate_training_data.py
+++ src/generate_training_data.py
@@ -1,27 +1,29 @@
-import gym
 import collections
 from statistics import mean, median
 import numpy as np
 
-env = gym.make('CartPole-v0')
-random_games = 20000
-maximum_steps = 500
-minimum_score = 90
 
-
-def initial_data():
+def initial_data(env, random_games, maximum_steps, minimum_score):
+    # all the scores
+    scores = []
+    # only scores that go beyond the minimum_score
     accepted_scores = []
-    game_memory = []
+    # [OBS, Moves]
     training_data = []
+
     for each_game in range(random_games):
         env.reset()
-        scores = []
+        # [OBS, Moves] for this game
+        game_memory = []
         score = 0
         previous_observation = []
 
         for _ in range(maximum_steps):
+            # choose a random action in action space
             action = env.action_space.sample()
+            # take the action and get relevant info
             observation, reward, done, info = env.step(action)
+            # append the [previous_observation, action] pair to memory
             if len(previous_observation) > 0:
                 game_memory.append([previous_observation, action])
             score += reward
@@ -34,7 +36,7 @@ def initial_data():
             for data in game_memory:
                 output = []
                 action_taken = data[1]
-
+                # one-hot encoding for action output
                 for action_index in range(env.action_space.n):
                     if action_index == action_taken:
                         output.append(1)
@@ -52,5 +54,3 @@ def initial_data():
     print('Median accepted score:', median(accepted_scores))
     print(collections.Counter(accepted_scores))
     return training_data
-
-initial_data()
